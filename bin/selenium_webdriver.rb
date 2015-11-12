c18b38b1d349c517777762ac88612102ad567a15
Sue Uyetake <sue.uyetake@gmail.com>
2015-11-12 06:50:34 -0700

    cleaned up code comments and informative output
    ready to split my own standalone code off to another project:
    
       selenium_webdriver.rb

--- bin/selenium_webdriver.rb
+++ bin/selenium_webdriver.rb
@@ -2,6 +2,58 @@
 require 'rubygems'
 require 'selenium-webdriver'
 
+# This standalone program will enter keywords
+# to a public search engine and seek the lat/long coordinates
+
+# unless a gemfile is used and bundled,
+# first install the following gems at the commandline:
+# gem install rubygems
+# gem install selenium-webdriver
+# gem install chromedriver-helper
+
+driver = Selenium::WebDriver.for :chrome
+driver.navigate.to "http://www.bing.com"
+
+# element can be found by name or by id
+# element = driver.find_element(:name, 'q')
+element = driver.find_element(:id, 'sb_form_q')
+
+# to ensure results, include lat long in the search string
+element.send_keys "Westminster, CO lat long"
+element.submit
+
+puts ''
+puts "Page Title: #{driver.title}"
+puts "Element of text box used was: #{element.inspect}"
+puts ''
+
+# seek the results and within the element containing lat long
+results = driver.find_element(:id, 'b_results')
+lat_long = results.find_element(:class, 'b_focusTextMedium')
+
+# puts results.inspect
+puts "Here is everything on the return page: "
+puts results.text
+
+# puts lat_long.inspect
+puts ''
+puts 'Here is what you really want to compare:'
+puts lat_long.text
+
+# comment out the quit to see the browser after run
+# ..yet without this you must manually quit the browser
+# driver.quit
+
+
+
+## future dev to add:
+# require 'geocoder'
+# and/or gem install geocoder
+# geocoded_by :address
+# after_validation :geocode
+
+
+## past development experiments and experiences:
 # driver = Selenium::WebDriver.for :chrome
 # driver.get "http://localhost:3000"
 #
@@ -48,18 +100,4 @@ require 'selenium-webdriver'
 # element.send_keys "Hello WebDriver!"
 # element.submit
 # puts driver.title
-# driver.quit
-
-driver = Selenium::WebDriver.for :chrome
-driver.navigate.to "http://www.bing.com"
-# element = driver.find_element(:name, 'q')
-element = driver.find_element(:id, 'sb_form_q')
-element.send_keys "Westminster, CO lat long"
-element.submit
-puts driver.title
-puts element.inspect
-result = driver.find_element(:id, 'b_results')
-puts result.inspect
-puts result.text.chomp()
-# puts result.attribute(b_focusTextMedium)
 # driver.quit
